d9ad1ca1f4552cf51ab9ca208682ab9692608081
Francisco Huerta <frhuerta.dwfsc16@ucamp.utel.edu.mx>
2023-06-10 22:34:15 -0600

    proyecto4

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
+
+const App = () => {
+  return (
+    <>
+    <HomePage />
+    <AboutPage />
+    </>
+  );
+};
+
+export default App;
